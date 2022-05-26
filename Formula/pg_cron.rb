f4a1d2cc19fdcee3e318a9d503aaba5979246deb
Michael Tesch <mtesch@utilimarc.com>
2022-05-25 23:32:08 -0500

    initial formula for pg_cron postgresql extension
    this adds pg_cron extension to postgresql.

---
+++ Formula/pg_cron.rb
@@ -0,0 +1,52 @@
+class PgCron < Formula
+  desc "Run periodic jobs in PostgreSQL"
+  homepage "https://github.com/citusdata/pg_cron"
+  url "https://github.com/citusdata/pg_cron/archive/refs/tags/v1.4.1.tar.gz"
+  sha256 "f50792baa7105f7e5526ec0dca6d2328e4290131247c305cc84628cd864e9da2"
+  license "PostgreSQL"
+
+  depends_on "postgresql"
+
+  def install
+    system "make"
+    (prefix/"lib/postgresql").install "pg_cron.so"
+    (prefix/"share/postgresql/extension").install "pg_cron--1.0.sql"
+    (prefix/"share/postgresql/extension").install "pg_cron--1.0--1.1.sql"
+    (prefix/"share/postgresql/extension").install "pg_cron--1.1--1.2.sql"
+    (prefix/"share/postgresql/extension").install "pg_cron--1.2--1.3.sql"
+    (prefix/"share/postgresql/extension").install "pg_cron--1.3--1.4.sql"
+    (prefix/"share/postgresql/extension").install "pg_cron.control"
+  end
+
+  def caveats
+    <<~EOS
+      To complete the installation, load pg_cron in postgresql.conf:
+        shared_preload_libraries = 'pg_cron'
+
+      Restart and then create the extension in the database:
+        -- run as superuser:
+        CREATE EXTENSION pg_cron;
+      For more details, read:
+        https://github.com/citusdata/pg_cron
+    EOS
+  end
+
+  test do
+    # Testing steps:
+    # - create new temporary postgres database
+    system "pg_ctl", "initdb", "-D", testpath/"test" unless ENV["HOMEBREW_GITHUB_ACTIONS"]
+
+    # - enable pg_cron in temporary database
+    File.write(testpath/"test/postgresql.conf", "\nshared_preload_libraries = 'pg_cron'\n", mode: "a+")
+    File.write(testpath/"test/postgresql.conf", "\nport = 5562\n", mode: "a+")
+
+    # - restart temporary postgres
+    system "pg_ctl", "start", "-D", testpath/"test", "-l", testpath/"log" unless ENV["HOMEBREW_GITHUB_ACTIONS"]
+
+    # - run "CREATE EXTENSION pg_cron;" in temp database
+    system "psql", "-p", "5562", "-c", "CREATE EXTENSION pg_cron;", "postgres" unless ENV["HOMEBREW_GITHUB_ACTIONS"]
+
+    # - shutdown temp postgres
+    system "pg_ctl", "stop", "-D", testpath/"test" unless ENV["HOMEBREW_GITHUB_ACTIONS"]
+  end
+end
