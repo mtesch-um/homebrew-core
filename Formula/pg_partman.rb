9f7701c07b5edbd344b4b0f514a11f7baf398c65
Michael Tesch <mtesch@utilimarc.com>
2022-05-26 13:59:21 -0500

    update with suggestions from pg_cron PR

--- Formula/pg_partman.rb
+++ Formula/pg_partman.rb
@@ -14,33 +14,22 @@ class PgPartman < Formula
     (prefix/"share/postgresql/extension").install Dir["updates/pg_partman--*.sql"]
   end
 
-  def caveats
-    <<~EOS
-      To complete the installation, load pg_partman_bgw in postgresql.conf:
-        shared_preload_libraries = 'pg_partman_bgw'
-
-      Restart and then create the extension in the database:
-        -- run as superuser:
-        CREATE EXTENSION pg_partman_bgw;
-      For more details, read:
-        https://github.com/pgpartman/pg_partman
-    EOS
-  end
-
   test do
     # Testing steps:
     # - create new temporary postgres database
     system "pg_ctl", "initdb", "-D", testpath/"test"
 
+    port = free_port
     # - enable pg_partman in temporary database
     File.write(testpath/"test/postgresql.conf", "\nshared_preload_libraries = 'pg_partman_bgw'\n", mode: "a+")
-    File.write(testpath/"test/postgresql.conf", "\nport = 5562\n", mode: "a+")
+    File.write(testpath/"test/postgresql.conf", "\nport = #{port}\n", mode: "a+")
 
     # - restart temporary postgres
     system "pg_ctl", "start", "-D", testpath/"test", "-l", testpath/"log"
 
     # - create extension in temp database
-    system "psql", "-p", "5562", "-c", "CREATE SCHEMA partman; CREATE EXTENSION pg_partman SCHEMA partman;", "postgres"
+    system "psql", "-p", port.to_s,
+           "-c", "CREATE SCHEMA partman; CREATE EXTENSION pg_partman SCHEMA partman;", "postgres"
 
     # - shutdown temp postgres
     system "pg_ctl", "stop", "-D", testpath/"test"
